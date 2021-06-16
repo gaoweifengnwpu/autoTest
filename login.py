8bad2d539ddaf7b2533f5f75f7b8e7b91b5ade75
gaoweifeng <gao_weifeng2@uxsino.com>
2021-06-16 10:46:08 +0800

    v1.0

---
+++ login.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+from time import sleep
+#1.创建Chrome浏览器对象，这会在电脑上在打开一个浏览器窗口
+browser = webdriver.Chrome()
+
+#2.通过浏览器向服务器发送URL请求
+browser.get("https://192.73.0.127:3000/login.html")
+#3.刷新浏览器
+# browser.refresh()
+login=['secur','secur@123']
+#4.设置浏览器的大小
+browser.set_window_size(1400,800)
+# 安全员登录
+browser.find_element_by_xpath("//*[@id='details-button']").click()
+browser.find_element_by_xpath("//*[@id='proceed-link']").click()
+browser.find_element_by_xpath("//*[@id='name']").send_keys(login[0])
+browser.find_element_by_xpath("//*[@id='pwd']").send_keys(login[1])
+browser.find_element_by_xpath("//*[@id='login']").click()
