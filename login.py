6bcc3d099704539d7413c9ca434bb2cb016d817e
gaoweifeng <gao_weifeng2@uxsino.com>
2021-06-16 15:35:29 +0800

    update

--- login.py
+++ login.py
@@ -17,3 +17,12 @@ browser.find_element_by_xpath("//*[@id='proceed-link']").click()
 browser.find_element_by_xpath("//*[@id='name']").send_keys(login[0])
 browser.find_element_by_xpath("//*[@id='pwd']").send_keys(login[1])
 browser.find_element_by_xpath("//*[@id='login']").click()
+# 当frame不存在id或者class时，可以用xpath的方式进行定位
+# iframe = find_element_by_xpath（"//div/iframe"）
+# switch_to_frame(iframe)
+browser.switch_to.frame("mainFrame")
+browser.find_element_by_xpath("//*[@name='searchContext' and @id='searchIp']").send_keys("sss")
+browser.find_element_by_xpath("//*[@class='iptext']").click()
+#跳出
+browser.switch_to.default_content()
+
