ad942cae6c6d16eee7b86c267ed28c31c0e8aa1d
gaoweifeng <gao_weifeng2@uxsino.com>
2021-06-23 16:15:15 +0800

    update

---
+++ 16转10.py
@@ -0,0 +1,7 @@
+str='c8e9aca0c6f2e5f3e8c4efe7a1a0d4e8e5a0e6ece1e7a0e9f3baa0e8eafae3f9e4eafae2eae4e3eaebfaebe3f5e7e9f3e4e3e8eaf9eaf3e2e4e6f2'
+flag=''
+for i in range(0,len(str),2):
+    str1=str[i]+str[i+1]
+    value = int(str1,16)
+    flag=flag+chr(value-128)
+print(flag)
